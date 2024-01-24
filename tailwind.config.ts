ffd611cb48adaa43d78b26770349c297b1287455
imopbuilder <opbuilder9@gmail.com>
2024-01-24 20:17:59 +0530

    feat: add loading-ui to code-editor

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -62,10 +62,14 @@ const config = {
 					from: { height: 'var(--radix-accordion-content-height)' },
 					to: { height: '0' },
 				},
+				'atom-loader': {
+					to: { transform: 'rotate(360deg)' },
+				},
 			},
 			animation: {
 				'accordion-down': 'accordion-down 0.2s ease-out',
 				'accordion-up': 'accordion-up 0.2s ease-out',
+				'atom-loader': 'atom-loader 1s linear infinite',
 			},
 			minHeight: {
 				hvh: 'calc(100vh - 65px)',
